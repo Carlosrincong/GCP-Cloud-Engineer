12c4e651f80207c4a59591f109a0ab6d10aff899
Carlos.Rincon <carlesterincgarc@gmail.com>
2024-02-16 17:01:18 -0500

    # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   IAM/README.md #	new file:   IAM/authn-tree.svg #	modified:   IAM/command.sh #

--- IAM/README.md
+++ IAM/README.md
@@ -9,3 +9,29 @@ Es la preferida para usar proveedor de identidad externo (IdP). No requiere la s
 
 ## Identidades para cargas de trabajo
 Workload identity en GKE, Cuenta de servicio y Clave de cuenta de servicio. 
+
+## Best practices:
+
+- Emplearla cuando se requiere que una aplicacion funcione sin interaccion de un usuario.
+- Que dicha cuenta solo tenga permisos para el alcance de lo que la aplicacion requiere para funcionar. 
+- Emplearla para autenticar identidades con la aplicacion o ante Google Cloud. 
+- Usar el prefijo del nombre de la cuenta de servicio para identificar su uso.
+- Crear cuentas de servicion de unico proposito
+- Rotar frecuentemente las claves de las cuentas de servicio
+- Deshabilitar cuentas de servicio que no se usan
+- Antes de borrar una cuenta de servicio, deshabilitarla
+- Evita usar grupos para otorgar a las cuentas de servicio acceso a los recursos.
+- Inhabilita el otorgamiento automático de IAM para las cuentas de servicio predeterminadas y asi no obtendran el rol de editor. 
+De este modo se le debe asignar manualmente los permisos a estas cuentas de servicio 
+- Evita usar la delegación de todo el dominio 
+- Usa la API de Credentials de IAM para la elevación de privilegios temporales
+- Estadísticas de movimiento lateral para rastrear cuenta del proyecto A que se hace pasar por cuenta del proyecto B y asi sucesivamente
+- Evita permitir que los usuarios actúen en nombre de las cuentas de servicio que tienen más privilegios que ellos. A traves de servicios a los que estos tienen acceso y a cuentas de servicio predeterminadas
+- No permitas que los usuarios creen o suban claves de cuenta de servicio.
+- Evitar que usuarios puedan elevar sus privilegios con cuentas de servicio, claves de cuentas de servicio o cuentas de servicio predeterminadas.
+- Riesgos cuenta de servicio: elevacion de privilegios y falsificacion de identidad.
+- Evita permitir que las canalizaciones de implementación administren los controles de seguridad o modifique las políticas.
+- Usa estadísticas y métricas para identificar claves de cuenta de servicio sin usar.
+- Rota las claves de la cuenta de servicio para reducir el riesgo de seguridad causado por claves filtradas.
+
+https://cloud.google.com/iam/docs/migrate-from-service-account-keys?hl=es-419
--- IAM/command.sh
+++ IAM/command.sh
@@ -9,6 +9,82 @@ gcloud projects add-iam-policy-binding nombre-proyecto --member="user:EMAIL_ADDR
 # Donde ROLE es un rol predefinido individual
 # Donde member puede ser: user, group, serviceAccount, domain. 
 
+## -------- Roles --------
+
+# Para obtener mas detalle sobre un rol predefinido
+gcloud iam roles describe roles/accessapproval.invalidator
+
+# Lista de roles que se pueden otorgar para un servicio:
+gcloud iam list-grantable-roles full-resource-name
+
+# Lista de permisos que se pueden otorgar para un servicio:
+gcloud iam list-testable-permissions FULL_RESOURCE_NAME \
+    --filter="customRolesSupportLevel!=NOT_SUPPORTED"
+
+## -------- Roles personalizados  -----------
+
+# Crear un rol personalizado YAML
+# A nivel de organizacion
+gcloud iam roles create ROLE_ID --organization=ORGANIZATION_ID \
+    --file=YAML_FILE_PATH.yaml
+# A nivel de proyecto:
+gcloud iam roles create ROLE_ID --project=nombre-proyecto \
+    --file=YAML_FILE_PATH
+# Crear el rol presupone crear un archivo YAML que contiene la informacion del rol personalizado:
+[
+title: "My Company Admin"
+description: "My custom role description."
+stage: "ALPHA"
+includedPermissions:
+- iam.roles.get
+- iam.roles.list
+]
+
+# Crear un rol personalizado con marcas:
+# A nivel de organizacion
+gcloud iam roles create ROLE_ID--organization=ORGANIZATION_ID \
+    --title=ROLE_TITLE --description=ROLE_DESCRIPTION \
+    --permissions="PERMISSIONS_LIST" --stage=LAUNCH_STAGE
+# A nivel de proyecto:
+gcloud iam roles create ROLE_ID --project=nombre-proyecto \
+    --title=ROLE_TITLE --description=ROLE_DESCRIPTION \
+    --permissions="PERMISSIONS_LIST" --stage=LAUNCH_STAGE
+
+# Donde PERMISSIONS_LIST es la lista de permisos separados por coma (,)
+
+# Actualizar rol a nivel de organizacion
+gcloud iam roles update ROLE_ID --organization=ORGANIZATION_ID \
+    --title=ROLE_TITLE --description=ROLE_DESCRIPTION \
+    --stage=LAUNCH_STAGE
+
+# Actualizar rol a nivel de proyecto
+gcloud iam roles update ROLE_ID --project=nombre-proyecto \
+    --title=ROLE_TITLE --description=ROLE_DESCRIPTION \
+    --stage=LAUNCH_STAGE
+
+# Deshabilitar rol personalizado a nivel de organizacion
+gcloud iam roles update ROLE_ID --organization=ORGANIZATION_ID \
+    --stage=DISABLED
+# Deshabilitar rol personalizado a nivel de proyecto
+gcloud iam roles update ROLE_ID --project=nombre-proyecto \
+    --stage=DISABLED
+
+# Enlistar roles a nivel de organizacion
+gcloud iam roles list --organization=ORGANIZATION_ID
+# Enlistar roles a nivel de proyecto
+gcloud iam roles list --project=nombre-proyecto
+
+# Borrar roles a nivel de organizacion
+gcloud iam roles delete ROLE_ID --organization=ORGANIZATION_ID
+# Borrar roles a nivel de proyecto
+gcloud iam roles delete ROLE_ID --project=nombre-proyecto
+
+# Recuperar roles a nivel de organizacion
+gcloud iam roles undelete ROLE_ID --organization=ORGANIZATION_ID
+# Recuperar roles a nivel de proyecto
+gcloud iam roles undelete ROLE_ID --project=nombre-proyecto
+
+## -------- Cuentas de servicio -----------
 gcloud iam service-accounts list ## Devuelve la lista de las cuentas de servicio creadas
 
 ## Crea la cuenta de servicio 'SA_NAME@PROJECT_ID.iam.gserviceaccount.com', asi: 
@@ -26,3 +102,32 @@ gcloud iam service-accounts add-iam-policy-binding \
     SA_NAME@nombre-proyecto.iam.gserviceaccount.com \
     --member="user:USER_EMAIL" \
     --role="roles/iam.serviceAccountUser"
+
+# Habilitar y deshabilitar cuentas de servicio:
+gcloud iam service-accounts disable SA_NAME@nombre-proyecto.iam.gserviceaccount.com
+gcloud iam service-accounts enable SA_NAME@nombre-proyecto.iam.gserviceaccount.com
+
+# Borrar cuenta de servicio
+gcloud iam service-accounts delete SA_NAME@nombre-proyecto.iam.gserviceaccount.com
+
+## --------- Claves de cuenta de servicio ------------
+## Crear clave de cuenta de servicio:
+gcloud iam service-accounts keys create KEY_FILE \
+    --iam-account=SA_NAME@nombre-proyecto.iam.gserviceaccount.com
+# donde KEY_FILE es la ruta del archivo json en el que se almacena la clave privada
+
+## Borrar clave de cuenta de servicio:
+gcloud iam service-accounts keys delete KEY_ID \
+    --iam-account=SA_NAME@nombre-proyecto.iam.gserviceaccount.com
+# Donde KEY_ID es el id de la clave.
+## Para saber el id de la clave se puede enlistar las claves de una cuenta de servicio, asi:
+gcloud iam service-accounts keys list \
+    --iam-account=SA_NAME@nombre-proyecto.iam.gserviceaccount.com
+
+# Habilitar y deshabilitar claves de cuenta de servicio:
+gcloud iam service-accounts keys disable KEY_ID \
+    --iam-account=SA_NAME@nombre-proyecto.iam.gserviceaccount.com \
+    --project=nombre-proyecto
+gcloud iam service-accounts keys enable KEY_ID \
+    --iam-account=SA_NAME@nombre-proyecto.iam.gserviceaccount.com\
+    --project=nombre-proyecto
