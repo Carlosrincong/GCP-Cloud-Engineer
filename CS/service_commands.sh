419b73d1837ce03651597b47bba1c122ac8f2190
Carlos.Rincon <carlesterincgarc@gmail.com>
2024-02-21 15:55:24 -0500

    # Changes to be committed: #	modified:   CS/service_commands.sh

--- CS/service_commands.sh
+++ CS/service_commands.sh
@@ -1,6 +1,31 @@
 
 # Create bucket
 gcloud storage buckets create gs://bucket-name --location=us-east1
+gsutil mb gs://bucket-name
+
+# Upload files to bucket
+gsutil cp ada.jpg gs://bucket-name
+
+# Download file from bucket to cloud shell
+gsutil cp -r gs:///bucket-name/file_to_download.jpg .
+
+# Copy file into a folder
+gsutil cp gs://bucket-name/file_to_copy.jpg gs://bucket-name/destination-folder/
+
+# List of files in bucket
+gsutil ls gs://bucket-name
+
+# Get metadata of a file
+gsutil ls -l gs://bucket-name/file_name.jpg
+
+# Grant public access
+gsutil acl ch -u AllUsers:R gs://bucket-name/file_name.jpg
+# Remove public access
+gsutil acl ch -d AllUsers gs://YOUR-BUCKET-NAME/ada.jpg
+
+# Delete files of bucket
+gsutil rm gs://bucket-name/file_name.jpg
 
 # Delete bucket
 gcloud storage rm --recursive gs://bucket-name
+
