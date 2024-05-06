65b2eabfa4e939d6550c81597b725ccd52bdcca3
Carlos.Rincon <carlesterincgarc@gmail.com>
2024-05-05 22:55:56 -0500

    # Changes to be committed: #	modified:   VPC/cloud_HA_VPN_gateways.sh #	new file:   img/cloud_HA_VPN_gateways.png

--- VPC/cloud_HA_VPN_gateways.sh
+++ VPC/cloud_HA_VPN_gateways.sh
@@ -53,7 +53,7 @@ export REGION=region
 gcloud compute networks subnets create $ONPREM_VPC_subnet \
  --network $ONPREM_VPC --range 192.168.1.0/24 --region $REGION
 
-# Create a firewall rule to allow all custom traffic within the networ
+# Create a firewall rule to allow all custom traffic within the network
 export ONPREM_FIREWALL_IN_RULE=on-prem-allow-custom
 gcloud compute firewall-rules create $ONPREM_FIREWALL_RULE \
  --network $ONPREM_VPC \
@@ -143,59 +143,103 @@ gcloud compute vpn-tunnels create $ONPREM_TUNNEL1 \
 
 # Create the interfaces in each VPN gateway. 
 # Iterfaces are the tunnel connection point inside the VPN gateway to facilitate the traffic through the tunnel 0 or 1
-gcloud compute routers add-interface vpc-demo-router1 \
-    --interface-name if-tunnel0-to-on-prem \
+export ONPREM_IF_0=if-tunnel0-to-on-prem
+gcloud compute routers add-interface $VPC_ROUTER \
+    --interface-name $ONPREM_IF_0 \
     --ip-address 169.254.0.1 \
     --mask-length 30 \
-    --vpn-tunnel vpc-demo-tunnel0 \
+    --vpn-tunnel $VPC_TUNNEL0 \
     --region $REGION
 
-gcloud compute routers add-interface vpc-demo-router1 \
-    --interface-name if-tunnel1-to-on-prem \
+export ONPREM_IF_1=if-tunnel1-to-on-prem
+gcloud compute routers add-interface $VPC_ROUTER \
+    --interface-name $ONPREM_IF_1 \
     --ip-address 169.254.1.1 \
     --mask-length 30 \
-    --vpn-tunnel vpc-demo-tunnel1 \
+    --vpn-tunnel $VPC_TUNNEL1 \
     --region $REGION
 
-gcloud compute routers add-interface on-prem-router1 \
-    --interface-name if-tunnel0-to-vpc-demo \
+export VPC_IF_0=if-tunnel0-to-vpc-demo
+gcloud compute routers add-interface $ONPREM_ROUTER \
+    --interface-name $VPC_IF_0 \
     --ip-address 169.254.0.2 \
     --mask-length 30 \
-    --vpn-tunnel on-prem-tunnel0 \
+    --vpn-tunnel $ONPREM_TUNNEL0 \
     --region $REGION
 
-gcloud compute routers add-interface  on-prem-router1 \
-    --interface-name if-tunnel1-to-vpc-demo \
+export VPC_IF_1=if-tunnel1-to-vpc-demo
+gcloud compute routers add-interface $ONPREM_ROUTER \
+    --interface-name $VPC_IF_1 \
     --ip-address 169.254.1.2 \
     --mask-length 30 \
-    --vpn-tunnel on-prem-tunnel1 \
+    --vpn-tunnel $ONPREM_TUNNEL01 \
     --region $REGION
 
 # Create Border Gateway Protocol (BGP) peer for each tunnel (0 and 1) in each networks
-gcloud compute routers add-bgp-peer vpc-demo-router1 \
+gcloud compute routers add-bgp-peer $VPC_ROUTER \
     --peer-name bgp-on-prem-tunnel0 \
-    --interface if-tunnel0-to-on-prem \
+    --interface $ONPREM_IF_0\
     --peer-ip-address 169.254.0.2 \
     --peer-asn 65002 \
     --region $REGION
 
-gcloud compute routers add-bgp-peer vpc-demo-router1 \
+gcloud compute routers add-bgp-peer $VPC_ROUTER \
     --peer-name bgp-on-prem-tunnel1 \
-    --interface if-tunnel1-to-on-prem \
+    --interface $ONPREM_IF_1 \
     --peer-ip-address 169.254.1.2 \
     --peer-asn 65002 \
     --region $REGION
 
-gcloud compute routers add-bgp-peer on-prem-router1 \
+gcloud compute routers add-bgp-peer $ONPREM_ROUTER \
     --peer-name bgp-vpc-demo-tunnel0 \
-    --interface if-tunnel0-to-vpc-demo \
+    --interface $VPC_IF_0 \
     --peer-ip-address 169.254.0.1 \
     --peer-asn 65001 \
     --region $REGION
 
-gcloud compute routers add-bgp-peer  on-prem-router1 \
+gcloud compute routers add-bgp-peer $ONPREM_ROUTER \
     --peer-name bgp-vpc-demo-tunnel1 \
-    --interface if-tunnel1-to-vpc-demo \
+    --interface $VPC_IF_1 \
     --peer-ip-address 169.254.1.1 \
     --peer-asn 65001 \
     --region $REGION
+
+# Get details about Cloud Routers
+gcloud compute routers describe $VPC_ROUTER \
+ --region $REGION
+
+gcloud compute routers describe $ONPREM_ROUTER \
+ --region $REGION
+
+# Define Firewall rules to allow all traffic between the two networks
+gcloud compute firewall-rules create vpc-demo-allow-subnets-from-on-prem \
+ --network $VPC \
+ --allow tcp,udp,icmp \
+ --source-ranges 192.168.1.0/24
+
+gcloud compute firewall-rules create on-prem-allow-subnets-from-vpc-demo \
+ --network $ONPREM_VPC \
+ --allow tcp,udp,icmp \
+ --source-ranges 10.1.1.0/24,10.2.1.0/24
+
+# Get status of tunnels
+gcloud compute vpn-tunnels list
+gcloud compute vpn-tunnels describe $VPC_TUNNEL0 --region $REGION # for specific tunnel
+
+# Verify connectovity over VPN 
+# SSH On prem instance and ping VPC IP address to reach instances in this network:
+ping -c 4 10.1.1.2
+
+# Update VPC routing to Global
+gcloud compute networks update $VPC --bgp-routing-mode GLOBAL
+# Verify tha change
+gcloud compute networks describe $VPC
+
+# Verify if the traffic is sent even if one tunnel is down
+# Bring a tunnel down
+gcloud compute vpn-tunnels delete $VPC_TUNNEL0  --region $REGION
+# Verify that the tunnel is down
+gcloud compute vpn-tunnels describe $ONPREM_TUNNEL0  --region $REGION
+# Now send traffic and see how you get response 
+ping -c 3 10.1.1.2
+
