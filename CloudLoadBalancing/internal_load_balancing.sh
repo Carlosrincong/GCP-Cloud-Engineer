65577bbe57eb081528b28a0ac7ca018ced64bfba
Carlos.Rincon <carlesterincgarc@gmail.com>
2024-05-17 23:20:43 -0500

    # Changes to be committed: #	modified:   CloudLoadBalancing/internal_load_balancing.sh

--- CloudLoadBalancing/internal_load_balancing.sh
+++ CloudLoadBalancing/internal_load_balancing.sh
@@ -1,7 +1,7 @@
 
 # Create a custom network with two subnets
 # Create a firewall rule to allow traffic from any sources in backend which is in specific IP range
-# Create a health check rule (firewall rule to allow health checks)
+# Create a health check rule (firewall rule to allow health checks) to allow traffic from health check
 # Create Cloud NAT to works with instances without external IP address. 
 ## Therefore, outbound traffic will send through Cloud NAT from the instance and inbound through the load balancer
 # Create a instance templeates for instance group creating (as backend of load balancer)
