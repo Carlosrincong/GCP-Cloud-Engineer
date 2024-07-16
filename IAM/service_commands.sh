593e0ad68093fdf909e5258b1d441591df8c87f3
Carlos.Rincon <carlesterincgarc@gmail.com>
2024-07-15 23:04:23 -0500

    # Changes to be committed: #	modified:   IAM/service_commands.sh

--- IAM/service_commands.sh
+++ IAM/service_commands.sh
@@ -1,13 +1,18 @@
 gcloud init ## Cuando se ejecute desde CLI
 
 gcloud projects create nombre-proyecto ## Crear el proyecto en caso de que no exista
-gcloud config set projec nombre-proyecto ## Seleccionar el proyecto
+gcloud config set project nombre-proyecto ## Seleccionar el proyecto
 
 gcloud services enable cloudresourcemanager.googleapis.com ## Habilitar la API
 
 gcloud projects add-iam-policy-binding nombre-proyecto --member="user:EMAIL_ADDRESS" --role=ROLE ### Otorgar permisos a una identidad
 # Donde ROLE es un rol predefinido individual
 # Donde member puede ser: user, group, serviceAccount, domain. 
+
+# Get the full list of policy in the project/folder/organization
+gcloud projects get-iam-policy $PROJECT_ID 
+gcloud resource-manager folders get-iam-policy $FOLDER_ID 
+gcloud organizations get-iam-policy $ORGANIZATION_ID 
 
 ## -------- Roles --------
 
